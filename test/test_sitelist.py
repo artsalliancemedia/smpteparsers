bf8232fcced9d822eeed19be019f90a7a9343fc2
Leo Hemsted <leohemsted@gmail.com>
2013-07-29 15:23:11 +0100

    changed some fn names in tests

--- test/test_sitelist.py
+++ test/test_sitelist.py
@@ -75,61 +75,61 @@ class TestSiteListXMLParsing(unittest.TestCase):
 class TestSiteListDateHandling(unittest.TestCase):
     def test_dates(self):
         #normal good date
-        self.assertEqual(helper.datetime('2012-01-13T12:30:00+00:00'),
+        self.assertEqual(helper.get_datetime('2012-01-13T12:30:00+00:00'),
                          datetime(2012, 1, 13, 12, 30, 0))
         #bad date (month 13)
-        self.assertRaises(ValueError, helper.datetime, '2012-13-13T12:30:00+00:00')
+        self.assertRaises(ValueError, helper.get_datetime, '2012-13-13T12:30:00+00:00')
 
     def test_goodtimezones(self):        
         date = '2012-01-01T12:20:00'
         dt = datetime(2012, 1, 1, 12, 20, 0)
         #utc
-        self.assertEqual(helper.datetime(date + '-00:00'), dt)   
-        self.assertEqual(helper.datetime(date + '+00:00'), dt)   
+        self.assertEqual(helper.get_datetime(date + '-00:00'), dt)   
+        self.assertEqual(helper.get_datetime(date + '+00:00'), dt)   
 
         #positive timezone 
-        self.assertEqual(helper.datetime(date + '+01:30'),
+        self.assertEqual(helper.get_datetime(date + '+01:30'),
                          dt - timedelta(hours=1, minutes = 30))
         #negative timezone 
-        self.assertEqual(helper.datetime(date + '-01:30'),
+        self.assertEqual(helper.get_datetime(date + '-01:30'),
                          dt - timedelta(hours=-1, minutes = -30))
 
     def test_badtimezones(self):
         date = '2012-01-01T12:20:00'
         #invalid timezones
-        self.assertRaises(ValueError, helper.datetime, date + 'aaaaaa')
-        self.assertRaises(ValueError, helper.datetime, date + '+aa:aa')
-        self.assertRaises(ValueError, helper.datetime, date + '+15:a0')
+        self.assertRaises(ValueError, helper.get_datetime, date + 'aaaaaa')
+        self.assertRaises(ValueError, helper.get_datetime, date + '+aa:aa')
+        self.assertRaises(ValueError, helper.get_datetime, date + '+15:a0')
 
 class TestSiteListUnusualTimes(unittest.TestCase):
     def test_noMillis_noTimezone(self):
         # missing "+00:00" or similar
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00'),
                          datetime(2012, 1, 1, 12, 30, 0))
 
     def test_millis_timezones(self):
         # with millis appended - just flooring that value for the moment
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00.123+01:00'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00.123+01:00'),
                          datetime(2012, 1, 1, 11, 30, 1))
 
     def test_millis_noTimezone(self):
         # with millis and no tz
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00.123'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00.123'),
                          datetime(2012, 1, 1, 12, 30, 1))
 
 
     def test_noColonTimezone(self):
         #timezone as +0000
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00+0130'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00+0130'),
                          datetime(2012, 1, 1, 11, 00, 0))
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00-0130'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00-0130'),
                          datetime(2012, 1, 1, 14, 00, 0))
 
     def test_noMinutesTimezone(self):
         #timezone as +00 (no minutes)
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00+01'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00+01'),
                          datetime(2012, 1, 1, 11, 30, 0))
-        self.assertEqual(helper.datetime('2012-01-01T12:30:00-01'),
+        self.assertEqual(helper.get_datetime('2012-01-01T12:30:00-01'),
                          datetime(2012, 1, 1, 13, 30, 0))
 
 class TestSiteListFetchHandling(unittest.TestCase):
@@ -146,9 +146,9 @@ class TestSiteListFetchHandling(unittest.TestCase):
         </SiteList>
         """
 
-    datetimeA = helper.datetime('2011-04-07T12:10:01-00:00')
-    datetimeB = helper.datetime('2012-05-08T12:11:02-01:20')
-    datetimeC = helper.datetime('2013-06-09T12:12:03+03:40')
+    datetimeA = helper.get_datetime('2011-04-07T12:10:01-00:00')
+    datetimeB = helper.get_datetime('2012-05-08T12:11:02-01:20')
+    datetimeC = helper.get_datetime('2013-06-09T12:12:03+03:40')
 
     def setUp(self):
         self.sites = SiteListParser(self.goodxml)
