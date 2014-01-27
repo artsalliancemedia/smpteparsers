0c4e7fb8e936377828fca934512c3aa375505b46
Alex Latchford <alex.latchford@artsalliancemedia.com>
2014-01-27 12:25:09 +0000

    Add in some documentation.

--- playlist/__init__.py
+++ playlist/__init__.py
@@ -7,17 +7,42 @@ Not technically a SMPTE standard, just the format we use internally to represent
 within the TMS. Thought this was the best place for the parse alongside the others for easy discovery :)
 """
 
+# Used to encapsulate the error so we don't expose we're using jsonschema.
 class PlaylistValidationError(ValidationError):
     pass
 
 class Playlist(object):
     def __init__(self, playlist_contents=None, parse=True, validate=True):
+        """
+        Init a new playlist to parse
+
+        Args:
+            playlist_contents (optional, default=None) - The contents of the playlist to parse
+            parse (optional, default=True) - Whether to parse the contents immediately
+            validate (optional, default=True) - Whether to validate the parsed contents immediately
+
+        Returns:
+            void
+        """
         self.playlist_contents = playlist_contents
 
         if self.playlist_contents is not None and parse:
             self.parse(self.playlist_contents, validate=validate)
 
     def parse(self, playlist_contents=None, validate=True):
+        """
+        Parse a playlist dict
+
+        Args:
+            playlist_contents (optional, default=None) - The contents of the playlist to parse, if None 
+                is received it'll try to load the existing playlist contents if there is some.
+            validate (optional, default=True) - Whether to validate the parsed contents
+
+        Returns:
+            void
+        """
+
+        # Only update the playlist_contents if new contents are sent through otherwise take what was given in init()
         if playlist_contents is not None:
             self.playlist_contents = playlist_contents
 
@@ -25,7 +50,7 @@ class Playlist(object):
             try:
                 self.playlist_contents = json.loads(self.playlist_contents)
             except ValueError:
-                # Trigger if we send through something that isn't valid json
+                # Triggers if we send through something that isn't valid json
                 self.playlist_contents = {}
 
         if validate:
@@ -40,6 +65,15 @@ class Playlist(object):
             self.events.append(PlaylistEvent(**event))
 
     def validate(self, schema_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'schema.json')):
+        """
+        Validate the playlist contents
+
+        Args:
+            schema_path (optional, default='./schema.json') - The path to the schema to validate the playlist against.
+
+        Returns:
+            void
+        """
         with open(schema_path) as f:
             schema = f.read()
             schema = json.loads(schema)
